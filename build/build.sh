8e4f56b66a6079cf9a56980c319c7c1d253700a3
dankennedy <daniel.kennedy@cmacgroup.com>
2024-04-30 17:57:54 +0100

    fixed path in build script to docs folder and removed static web app config

--- build/build.sh
+++ build/build.sh
@@ -2,4 +2,4 @@
 set -e
 echo "Deploy - Static Web App"
 docker build -t deploycmacpartnerspa -f ./Dockerfile ../
-docker run -e "APP_PATH=/app/build" -e "API_TOKEN=$swatoken_cmacpartner" -e "OUTPUT_PATH=/app/build/docs/.vitepress/dist" -e "APP_BUILD_COMMAND='npm run build'" deploycmacpartnerspa
+docker run -e "APP_PATH=/app/build/docs" -e "API_TOKEN=$swatoken_cmacpartner" -e "OUTPUT_PATH=/app/build/docs/.vitepress/dist" -e "APP_BUILD_COMMAND='npm run build'" deploycmacpartnerspa
